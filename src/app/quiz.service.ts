5ab7de52440f1e0eb032e2b7f54e70f5fb166bd0
zyarr.innovation <zyarr.innovation@gmail.com>
2024-12-08 21:32:07 +0530

    Updated code

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,85 +1,88 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IQuestion, QuestionCollection } from './data/questionCollection'
-import { EMPTY, from, Observable, of } from "rxjs";
+import { IQuestion, QuestionCollection } from './data/questionCollection';
+import { EMPTY, from, Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuizService {
+  //---------------- Properties---------------
+  readonly rootUrl = window.location.origin;
+  //readonly rootUrl = "http://127.0.0.1:8080";
 
-//---------------- Properties---------------
-readonly rootUrl = window.location.origin;
-//readonly rootUrl = "http://127.0.0.1:8080";
+  qns: any[];
+  seconds: number;
+  timer: any;
+  qnProgress: number;
+  correctAnswerCount: number;
 
-qns: any[];
-seconds: number;
-timer: any;
-qnProgress: number;
-correctAnswerCount: number;
-
-//---------------- Helper Methods---------------
-constructor(private http: HttpClient,
-  private questionCollection: QuestionCollection) { 
+  //---------------- Helper Methods---------------
+  constructor(
+    private http: HttpClient,
+    private questionCollection: QuestionCollection
+  ) {
     this.qns = [];
     this.seconds = 0;
     this.qnProgress = 0;
     this.correctAnswerCount = 0;
   }
 
-displayTimeElapsed() {
-  return (
-    Math.floor(this.seconds / 3600) +
-    ":" +
-    Math.floor(this.seconds / 60) +
-    ":" +
-    Math.floor(this.seconds % 60)
-  );
-}
+  displayTimeElapsed() {
+    return (
+      Math.floor(this.seconds / 3600) +
+      ':' +
+      Math.floor(this.seconds / 60) +
+      ':' +
+      Math.floor(this.seconds % 60)
+    );
+  }
 
-getParticipantName() {
-  var participant = JSON.parse(localStorage.getItem("participant")!);
-  return participant.name;
-}
+  getParticipantName() {
+    var participant = JSON.parse(localStorage.getItem('participant')!);
+    return participant.name;
+  }
 
-insertParticipant(name: string, email: string) {
-  var body = {
-    name: name,
-    email: email
-  };
-  localStorage.setItem("participant", JSON.stringify(body))
-  //return this.http.post(this.rootUrl + "/registerParticipant", body);
-  return of(body);
-}
+  insertParticipant(name: string, email: string) {
+    var body = {
+      name: name,
+      email: email,
+    };
+    localStorage.setItem('participant', JSON.stringify(body));
+    //return this.http.post(this.rootUrl + "/registerParticipant", body);
+    return of(body);
+  }
 
-getParticipantList() {
-  return from([localStorage.getItem("participant")]);
-  //return this.http.get(this.rootUrl + "/participantList");
-}
+  getParticipantList() {
+    return from([localStorage.getItem('participant')]);
+    //return this.http.get(this.rootUrl + "/participantList");
+  }
 
-//---------------- Http Methods---------------
-getQuestions() {
-  // Return an observable of the questions
-  return new Observable<IQuestion[]>((observer) => {
-    // Use the get method from QuestionCollection
-    const questions = this.questionCollection.get();
-    observer.next(questions);
-    observer.complete();
-  });
-  //return this.http.get(this.rootUrl + "/questionList");
-}
+  //---------------- Http Methods---------------
+  getQuestions(): Observable<IQuestion[]> {
+    return new Observable<IQuestion[]>((observer) => {
+      this.questionCollection.get().subscribe({
+        next: (questions) => {
+          observer.next(questions);
+          observer.complete();
+        },
+        error: (err) => {
+          observer.error(err);
+        },
+      });
+    });
+  }
 
-// getAnswers() {
-//   var body = this.qns.map(x => x.Id);
-//   return this.http.post(this.rootUrl + "/answers", body);
-// }
+  // getAnswers() {
+  //   var body = this.qns.map(x => x.id);
+  //   return this.http.post(this.rootUrl + "/answers", body);
+  // }
 
-submitScore() {
-  var body = JSON.parse(localStorage.getItem("participant")!);
-  body.Score = this.correctAnswerCount;
-  body.TimeSpent = this.seconds;
-  //return this.http.post(this.rootUrl + "/updateScore", body);
-  return EMPTY;
+  submitScore() {
+    var body = JSON.parse(localStorage.getItem('participant')!);
+    body.Score = this.correctAnswerCount;
+    body.TimeSpent = this.seconds;
+    //return this.http.post(this.rootUrl + "/updateScore", body);
+    return EMPTY;
+  }
 }
-}
-
